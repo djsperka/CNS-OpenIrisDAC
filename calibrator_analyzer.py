f17873405ea594bd295ce373e945491aba7936c4
Daniel Sperka <djsperka@ucdavis.edu>
2026-07-15 11:58:19 -0700

    wip

--- calibrator_analyzer.py
+++ calibrator_analyzer.py
@@ -3,37 +3,71 @@ from scipy.optimize import curve_fit as curve_fit
 from open_iris_client import EyeData, EyesData, ExtraData, Point
 from dac_common import CalibrationParameters
 import math
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from dataclasses import dataclass
+from argparse import ArgumentParser
+from pathlib import Path
+from generator import FileEyeDataGenerator
+from matplotlib.backend_bases import MouseButton
+from typing import List
+from collections import defaultdict as defaultdict
+
+binding_id = None
+
+def on_move(event):
+    if event.inaxes:
+        print(f'data coords {event.xdata} {event.ydata},',
+              f'pixel coords {event.x} {event.y}')
+
+
+def on_click(event):
+    if event.button is MouseButton.LEFT:
+        print(f'data coords {event.xdata} {event.ydata},',
+              f'pixel coords {event.x} {event.y}')
 
 class TWrapperClass:
 
     def __init__(self, x_bias, y_bias):
         self.cal = CalibrationParameters(x_bias=x_bias, y_bias=y_bias,x_gain=1,y_gain=1,rotation=0)
     
+    def get_cal(self, x_gain, y_gain, degrees):
+        self.cal.x_gain = x_gain
+        self.cal.y_gain = y_gain
+        self.cal.rotation = degrees
+        return self.cal
+
     def do_transform(self, xy, x_gain, y_gain, degrees):
         self.cal.x_gain = x_gain
         self.cal.y_gain = y_gain
         self.cal.rotation = degrees
-        p = self.cal.transform(Point(xy[0], xy[1]))
-        # xy is an avg x,y crsig values
-        # return nd.array[xtrans,ytrans] from transform using params   
-        p = Point(xy[0] * x_gain, xy[1] * y_gain)
-        trans = p.rotate(degrees * math.pi / 180)
-        #print(f"xy {xy[0]},{xy[1]} {x_gain} {y_gain} {degrees} trans {trans.x},{trans.y}")
-        return np.array([trans.x, trans.y])
-
+        pp = self.cal.transform(Point(xy[0], xy[1]))
+        return np.array([pp.x, pp.y])
 
     def func(self, xydata, x_gain, y_gain, degrees):
         pdata = np.apply_along_axis(self.do_transform, 0, xydata, x_gain, y_gain, degrees)
-        print(pdata)
         return pdata.ravel()
 
 
 
+@dataclass
+class ButtonPressInfo:
+    ind: int
+    vx: float
+    vy: float
+    frameind: int
+    checked: bool
+
+
+
 class CalibratorAnalyzer():
-    def __init__(self, fps:int=500, initial_size_sec:int=1800, increase_step_sec:int = 300):
+    def __init__(self, fps:int=500, initial_size_sec:int=1800, increase_step_sec:int = 300, before_sec:float=0.1, after_sec:float=0.1, vmax_px_per_sec:float=5000, doplot=True):
         self._fps = fps
         self._max_frames = np.floor(initial_size_sec * fps)
         self._increase_step_frames = np.floor(increase_step_sec * fps)
+        self._nbefore = int(np.floor(before_sec * self._fps))
+        self._nafter = int(np.floor(after_sec * self._fps))
+        self._vmax = vmax_px_per_sec/self._fps
 
         # data arrays
         self._pupil_xy=np.zeros((2,self._max_frames))
@@ -41,13 +75,25 @@ class CalibratorAnalyzer():
         self._p4_xy=np.zeros((2,self._max_frames))
         self._pupil_xy=np.zeros((2,self._max_frames))
         self._pupil_xy=np.zeros((2,self._max_frames))
+
+        # good measurements saved here
+        self._meas = defaultdict(list)
         
         # these are for state management
         self._counter = 0
         self._button_up = True
-        self._button_list=[]  # (index,x,y)
+        self._button_list: List[ButtonPressInfo] = [] # (index,x,y)
         self._framesig_on = False
         self._framesig_on_at = 0
+
+        # plotting or not?
+        self._doplot = doplot
+        self._invalidated = False
+        if self._doplot:
+            plt.ion()
+            self._f, self._axes = plt.subplots(2, 1, figsize=(10,10))
+            self._paths = {}
+
 
     def step(self, ed: EyesData):
         data_ok = True
@@ -89,22 +135,53 @@ class CalibratorAnalyzer():
             if button_is_pressed:
                 self._button_up = False
                 if data_ok:
-                    self._button_list.append((self._counter, ed.extra.doubles[7], ed.extra.doubles[8], self._framesig_on_at))
+                    self._button_list.append(ButtonPressInfo(self._counter, ed.extra.doubles[7], ed.extra.doubles[8], self._framesig_on_at, False))
+
         else:
             if not button_is_pressed:
                 self._button_up = True
     
         self._counter += 1        
+        self._button_ana()
+        self._update_plot()
         return self._counter
 
-    def get_crsig(self):
-        return self._cr_xy[:,:self._counter]-self._pupil_xy[:,:self._counter]
+    def _update_plot(self):
+        if self._doplot and self._invalidated:
+            self._invalidated = False
 
-    def get_dpisig(self):
-        return self._cr_xy[:,:self._counter]-self._p4_xy[:,:self._counter]
+            # re-draw scatter plot(s)
+
+            # Each key in self._meas is a calibration point
+            print("===========================")
+            count = 0
+            for i, (key,pts) in enumerate(self._meas.items()):
+                print(f"{i} {str(key)}: {len(pts)}")
+                xy=np.stack(pts)
+                self._axes[0].scatter(xy[:,0], xy[:,1], color=cm.tab20(i))
+            plt.show()
+            plt.pause(0.1)
 
 
-    def _check_velocity(self, xy, ind, nbefore, nafter, vmax):
+    def get_crsig(self, start:None|int=None, stop:None|int=None, step:None|int=None):
+        """Get CR signal (cr-pupil) for the slice described by slice(start,stop,step). 
+
+        Args:
+            start (None | int, optional): see slice(). Defaults to None.
+            stop (None | int, optional): see slice(). Defaults to None.
+            step (None | int, optional): see slice(). Defaults to None.
+
+        Returns:
+            ndarray: row 0/1 is x/y value. shape=(2,M)
+        """
+        s=slice(start,stop,step)
+        return self._cr_xy[:,s]-self._pupil_xy[:,s]
+
+    def get_dpisig(self, start:None|int=None, stop:None|int=None, step:None|int=None):
+        s=slice(start, stop, step)
+        return self._cr_xy[:,s]-self._p4_xy[:,s]
+
+    def _check_velocity(self, xy, vmax):
         """Check that velocity (in x and y separately) is less than vmax in the range defined by ind, nbefore, and nafter.
 
         Args:
@@ -115,11 +192,27 @@ class CalibratorAnalyzer():
             vmax (float): max velocity 
         """
         v = np.diff(xy, axis=1)
-        return np.all(np.abs(v[:,ind-nbefore:ind+nafter]) < vmax)
+        return np.all(np.abs(v[:,:]) < vmax)
+
+
+    def _button_ana(self):
+        """Using the current button_list, perform checks on each entry that has not been checked."""
+        for binf in self._button_list:
+            if not binf.checked:
+                if binf.vx < 900 and binf.vy < 900:
+                    if binf.ind+self._nafter < self._counter:
+                        crsig = self.get_crsig(binf.ind-self._nbefore, binf.ind+self._nafter)
+                        if self._check_velocity(crsig, self._vmax):
+                            m = crsig.mean(axis=1)
+                            self._meas[(binf.vx,binf.vy)].append(m)
+                            self._invalidated = True
+                        binf.checked = True
+                else:
+                    binf.checked = True # weird initialization values here
 
 
 
-    def ana_cr(self, before_sec:float=0.1, after_sec:float=0.1, vmax_px_per_sec:float=5000):
+    def ana_cr(self, before_sec:float=0.1, after_sec:float=0.1, vmax_px_per_sec:float=5000,doplot=True):
         nbefore = int(np.floor(before_sec * self._fps))
         nafter = int(np.floor(after_sec * self._fps))
         vmax = vmax_px_per_sec/self._fps
@@ -131,7 +224,7 @@ class CalibratorAnalyzer():
 
         for (ind,vx,vy,f) in self._button_list:
             if ind+nafter < self._counter:
-                if self._check_velocity(crsig,ind,nbefore,nafter,vmax):
+                if np.abs(vx) < 100 and np.abs(vy) < 100 and self._check_velocity(crsig,ind,nbefore,nafter,vmax):
                     # save (0,0) separately - this will be offset value
                     m = np.mean(crsig[:,ind-nbefore:ind+nafter], axis=1)
                     if vx==0 and vy==0:
@@ -141,57 +234,89 @@ class CalibratorAnalyzer():
                         target_xy_list.append((vx, vy))
 
         bias = np.stack(bias_xy_list).mean(axis=0)
-        xdata=(np.stack(measured_xy_list)).T
-        ydata=np.stack(target_xy_list).ravel()
+        measured_xy = np.stack(measured_xy_list)
+        # measured_xy = measured_xy - bias
+        target_xy = np.stack(target_xy_list)
 
         # initial guess for x_gain, y_gain, degrees
         p0 = np.array([.2,.2,0])
 
         # limits for same
-        low_limits = np.array([0.1, 0.1, -90])
-        hi_limits = np.array([20, 20, 90])
+        low_limits = np.array([0.1, 0.1, -10])
+        hi_limits = np.array([10, 10, 10])
 
-        # fit
+
         wrapper = TWrapperClass(-bias[0], -bias[1])
-        popt, pcov = curve_fit(wrapper.func, xdata, ydata, p0, bounds=(low_limits, hi_limits))
+        popt, pcov = curve_fit(wrapper.func, measured_xy.T, target_xy.T.ravel(), p0, bounds=(low_limits, hi_limits))
         print("popt",popt)
         print("pcov",pcov)
 
-# # get transform of input data, plot it
-# pdata = transform_wrapper(xdata, popt[0], popt[1], popt[2])
-# print(f"pdata shape {np.shape(pdata)}")
-# result = pdata.reshape((2,pdata.shape[0]//2))
-# print(np.shape(result))
-# print(np.shape(xdata))
+        if doplot:
+            transformed = wrapper.func(measured_xy.T, popt[0], popt[1], popt[2])
+            transformed_xy = transformed.reshape(2, transformed.shape[0]//2)
+            self._do_plot_cr(measured_xy.T, transformed_xy, target_xy.T)
+
+    # def _do_plot_cr(self, xy_raw, xy_result, xy_target):
+    #     axes[0].scatter(xy_raw[0,:], xy_raw[1,:], color='blue', label='raw')
+    #     axes[0].set_xlabel('X')
+    #     axes[0].set_ylabel('Y')
+    #     axes[0].set_title('Raw CR signal')
+    #     axes[1].scatter(xy_result[0,:], xy_result[1,:], color='blue', label='transformed')
+    #     axes[1].set_xlabel('X')
+    #     axes[1].set_ylabel('Y')
+    #     f.tight_layout()
+
+    #     plt.connect('button_press_event', on_click)
+
+    #     plt.show()
 
 
+def event_printer(event):
+    """Helper function for exploring events.
 
-# f, axes = plt.subplots(2, 1, figsize=(10,10))
-# axes[0].scatter(xdata[0,:], xdata[1,:], color='blue', label='raw')
-# axes[0].set_xlabel('X')
-# axes[0].set_ylabel('Y')
-# axes[0].set_title('Raw CR signal')
-# axes[1].scatter(result[0,:], result[1,:], color='blue', label='transformed')
-# axes[1].set_xlabel('X')
-# axes[1].set_ylabel('Y')
-# f.tight_layout()
-# plt.show()
+    Prints all public attributes +
+    """
+    # capture the last event
+    # global last_ev
+    # last_ev = event
+    for k, v in sorted(vars(event).items()):
+        print(f'{k}: {v!r}')
+    print('-'*25)
 
+
+def on_key(event):
+    # This is _super_ useful for debugging!
+    print(event.key)
+
+    # # if the key is c (any case)
+    # if event.key.lower() == 'c':
+    #     # change the color
+    #     self.ln.set_color(next(self.color_cyle))
+
+    #     # ask the GUI to re-draw the next time it can
+    #     self.ln.figure.canvas.draw_idle()
 
 
 
 def main() -> None:
-    from generator import FileEyeDataGenerator
-    filename='./cal95.pkl'
-    ana = CalibratorAnalyzer()
-    g = FileEyeDataGenerator(filename)
+    # Single input argument (optional) is filename to write output to.
+    parser = ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+    ana = CalibratorAnalyzer(fps=500, initial_size_sec=1800, increase_step_sec=300, before_sec=0.1, after_sec=0.1, vmax_px_per_sec=5000, doplot=True)
+    cid = ana._f.canvas.mpl_connect('button_press_event', event_printer)
+    key_cid = ana._f.canvas.mpl_connect('key_press_event', on_key)
+
+
+    g = FileEyeDataGenerator(args.filename)
     for ed in g.generate():
         ana.step(ed)
-    print(f"crsig shape {str(np.shape(ana.get_crsig()))}")
-    ana.ana_cr(0.1,0.1,5000)
-    #print(measured_xy)
-    #print(target_xy)
-
+    print("loop done")
+    plt.ioff()
+    plt.show()
+    #plt.pause(5)
+    #ana.ana_cr(0.1,0.1,5000,doplot=True)
+    
 
 
 if __name__ == "__main__":
