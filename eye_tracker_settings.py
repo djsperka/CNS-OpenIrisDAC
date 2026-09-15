05108f0d74cfc09755b1e5585ea49ac298808215
Daniel Sperka <djsperka@ucdavis.edu>
2026-09-15 09:55:34 -0700

    Misc fixes to make this work right.

--- eye_tracker_settings.py
+++ eye_tracker_settings.py
@@ -6,17 +6,25 @@ import pprint
 
 class EyeTrackerSettings:
     def __init__(self, tracker_folder: str = ''):
+        """This class will get the frame rate from the settings file for the OpenIris eye tracker application. It can be modified to read other settings in that file -- I just need the frames per second.
+
+        Args:
+            tracker_folder (string, optional): Folder where the OpenIris.exe executable file lives. If not specified, there must be a process named 'OpenIris.exe' currently running. Defaults to ''.
+
+        Raises:
+            RuntimeError: If no folder specified and no running OpenIris.exe was found, or if no settings file 'EyeTrackerSettings.xml' is found there.
+        """
+
         # if tracker folder is not specified, look for exe to find it
-        if tracker_folder:
-            self.tracker_folder = tracker_folder
-        else:
+        self.tracker_folder = tracker_folder
+        if not self.tracker_folder:
             c = wmi.WMI()
             for process in c.Win32_Process(Name="OpenIris.exe"):
                 if process.ExecutablePath:
                     self.tracker_folder = os.path.dirname(process.ExecutablePath)
                     break
             if not self.tracker_folder:
-                raise RuntimeError("A running OpenIris executable was not found.")
+                raise RuntimeError("Tracker folder not specified, and a running OpenIris executable was not found.")
 
         # locate settings xml file, but don't open yet
         self.settings_file = os.path.join(self.tracker_folder, "EyeTrackerSettings.xml")
@@ -26,6 +34,17 @@ class EyeTrackerSettings:
             raise RuntimeError(f"Eye tracker settings file not found at {self.settings_file}")
         
     def get_frame_rate(self, system_type: str = 'Spinnaker Single Camera'):
+        """Get the frame capture rate for the given system type.
+
+        Args:
+            system_type (str, optional): One of the system types. A system type corresponds to a key to one of the items under 'AllEyeTrackerSystemSettings' in the settings file. Defaults to 'Spinnaker Single Camera'.
+
+        Raises:
+            RuntimeError: If the system type supplied is not found in the settings file.
+
+        Returns:
+            integer: Frame capture rate, in frames per second.
+        """
         i_rate = 0
         found_type = False
         with open(self.settings_file, "rb") as fp:
